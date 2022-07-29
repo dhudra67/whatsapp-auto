49b9ab28134cb37c1c2075100c80ad8d05f475c7
dhudra67 <muhammadfaizanali67@gmil.com>
2022-07-29 11:55:06 +0500

    AUTO SENDING A message on whatsapp.

---
+++ whatsapp-auto.py
@@ -0,0 +1,5 @@
+import pywhatkit
+number = input("Enter a Number: ")
+time_in_hours = input("Enter time in hours: ")
+time_in_mins = input("Enter time in minutes: ")
+send.whatsmsg(number , "HI" ,time_in_hours , time_in_mins ,10 ,True , 2 )
